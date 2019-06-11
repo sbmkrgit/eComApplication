c98ae022ad52a20ea751c6ac35fb3a987ff0fc37
SHUBHAM <babukumar9931@gmail.com>
2019-06-11 11:28:27 +0530

    Product Detail Added

---
+++ app/src/main/java/in/ac/sharda/productdetail/ProductDetailActivity.java
@@ -0,0 +1,15 @@
+package in.ac.sharda.productdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.ac.sharda.loginactivity.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
